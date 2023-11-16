0a4635f31e19545250b4539d6f63695eeaef34ac
Diego Collares <diego.collares@gmail.com>
2023-11-16 17:53:30 -0300

    create search_text and add pg_search gem

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem 'pg_search', '~> 2.3', '>= 2.3.6'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
+  include PgSearch::Model
   has_one_attached :photo
   validates :title, :description, :price, presence: true
 
   belongs_to :category
+
+  pg_search_scope :search_full_text, against: {
+    title: 'A',
+    description: 'B',
+  }
 end
